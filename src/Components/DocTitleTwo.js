7f386371cedad17f10c93ad2a323cf1284ab778f
aparnassn <aparnasarwa@gmail.com>
2023-12-30 18:03:35 +0530

    DocTitleOne

---
+++ src/Components/DocTitleTwo.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/esm/Button";
+import useDocumentTitle from "../Hooks/useDocumentTitle";
+
+function DocTitleTwo() {
+  const [count, setCount] = useState(0);
+
+  useDocumentTitle(count)
+
+  return (
+    <div>
+      <Button onClick={() => setCount(count + 1)}>Count-{count}</Button>
+    </div>
+  );
+}
+
+export default DocTitleTwo;
